2946b88fe871d36d098acb40e062f4f506244901
Garcia Sébastien <34031275+Sebestala@users.noreply.github.com>
2024-10-24 21:43:03 +0200

    Update config for matching with all domains

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
+// Create a domain list from 0 to 49
+const tumblrDomains = Array.from(
+  { length: 50 },
+  (_, i) => `${i}.media.tumblr.com`,
+);
+
 const nextConfig: NextConfig = {
   images: {
-    domains: ["24.media.tumblr.com"],
+    domains: tumblrDomains,
   },
   /* config options here */
 };
