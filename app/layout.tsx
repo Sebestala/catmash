1ef5d99b630f71cd35d8bc43307bfbbb8d9c0f4f
Garcia Sébastien <34031275+Sebestala@users.noreply.github.com>
2024-10-24 20:08:53 +0200

    Update responsive design for Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Image from "next/image";
 import Link from "next/link";
 import { ChevronUp } from "lucide-react";
+import ResponsiveCatImage from "@/components/ResponsiveCatImage";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -32,32 +32,28 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <header className="fixed top-0 z-50 w-full">
-          <div className="flex w-full flex-col items-center justify-center space-y-2 p-4">
-            <Image
-              src="/cat_topBar.webp"
-              alt="Happy Cat"
-              width={50}
-              height={50}
-              priority
-            />
+          <div className="flex w-full flex-col items-center justify-center space-y-2 bg-white p-4">
+            <ResponsiveCatImage />
             <h1 className="flex items-center justify-center text-lg font-bold text-blue-900 md:text-xl lg:text-2xl">
               CATMASH
             </h1>
           </div>
         </header>
-        <main className="container mx-auto flex-grow px-4 py-8">
+        <main className="container mx-auto mt-28 flex-grow px-4">
           {children}
         </main>
         <footer>
           <nav className="flex justify-center">
-            <div className="fixed -bottom-1 z-50 rounded-t-lg border-2 border-blue-950 md:w-1/2 md:max-w-md">
+            <div className="fixed -bottom-0.5 z-50 rounded-t-lg border border-blue-950 bg-white md:w-1/2 md:max-w-md">
               <Link href="/" className="h-full w-full">
-                <div className="flex flex-col items-center justify-center space-y-2 p-4">
-                  <ChevronUp size={28} strokeWidth={2.2} color="black" />
-                  <h2 className="text-base font-semibold text-blue-900">
+                <div className="flex flex-col items-center justify-center space-y-0.5 px-4 py-1 md:space-y-2">
+                  <ChevronUp size={22} strokeWidth={2.2} color="black" />
+                  <h2 className="text-sm font-semibold text-blue-900 md:text-lg lg:text-xl">
                     Voir le classement des chats
                   </h2>
-                  <p className="text-sm text-blue-800">X Matchs joués</p>
+                  <p className="text-sm text-blue-800 md:text-base lg:text-lg">
+                    X Matchs joués
+                  </p>
                 </div>
               </Link>
             </div>
