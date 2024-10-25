91dde0f169e7d6b53c9c8a8b7645f0b3ce228002
Garcia Sébastien <34031275+Sebestala@users.noreply.github.com>
2024-10-25 16:30:57 +0200

    Add background to TopCatBar

--- components/Layouts/TopCatBar.tsx
+++ components/Layouts/TopCatBar.tsx
@@ -2,11 +2,13 @@ import ResponsiveCatImage from "@/components/ResponsiveCatImage";
 
 export default function TopCatBar(): React.ReactElement {
   return (
-    <div className="flex w-full flex-col items-center justify-center space-y-2 p-4">
-      <ResponsiveCatImage />
-      <h1 className="text-md flex items-center justify-center font-bold text-blue-900 md:text-lg lg:text-xl">
-        CATMASH
-      </h1>
+    <div className="flex w-full items-center justify-center space-y-2 p-2">
+      <div className="flex h-full flex-col items-center justify-center rounded-xl bg-white p-2">
+        <ResponsiveCatImage />
+        <h1 className="text-md flex items-center justify-center font-bold text-blue-900 md:text-lg lg:text-xl">
+          CATMASH
+        </h1>
+      </div>
     </div>
   );
 }
