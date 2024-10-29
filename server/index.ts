bef645233a044089aae7ab95ae469740d56ec235
Garcia Sébastien <34031275+Sebestala@users.noreply.github.com>
2024-10-29 18:57:47 +0100

    Create server/index.ts

---
+++ server/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Bienvenue sur le serveur des chats!");
+});
