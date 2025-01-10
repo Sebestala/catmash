6021cb9b9479463595479f6b6bab0c2bc9452a62
Garcia Sébastien <34031275+Sebestala@users.noreply.github.com>
2025-01-10 09:11:00 +0100

    delete dist folder

--- apps/api/dist/api/config/supabase.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.supabase = void 0;
-const supabase_js_1 = require("@supabase/supabase-js");
-if (!process.env.SUPABASE_URL || !process.env.SUPABASE_KEY) {
-    throw new Error("Missing Supabase environment variables");
-}
-exports.supabase = (0, supabase_js_1.createClient)(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
